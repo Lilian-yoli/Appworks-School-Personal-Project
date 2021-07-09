2c84fe51a3cbd95ed589b2aa99e5136930cdf1e4
Lilian-yoli <lilian860919@gmail.com>
2021-07-09 22:39:25 +0800

    fix first chat bug

--- public/js/chat.js
+++ public/js/chat.js
@@ -27,8 +27,10 @@ window.onload = async () => {
         { src: "./uploads/images/Chat.png" }));
     } else if (data.firstSidebar && data.firstSidebar.length < 1) {
       createChatting(null, data.usersInfo, data.usersInfo.now);
-    } else if (!data.firstSidebar) {
+    } else if (!data.firstSidebar && data.sidebar.length > 0) {
       createChatting(data.sidebar, data.usersInfo, null);
+    } else if (!data.firstSidebar && data.sidebar.length == 0) {
+      createChatting(null, data.usersInfo, data.usersInfo.now);
     } else {
       createChatting(data.firstSidebar, data.usersInfo, null);
     }
@@ -56,13 +58,12 @@ window.onload = async () => {
     const receiverName = data.usersInfo.username2;
     const receiverPic = data.usersInfo.userPicture2;
     const room = data.usersInfo.room;
-    console.log("sender", senderId);
+
     socket.emit("login", senderId);
 
     send.addEventListener("click", (e) => {
       e.preventDefault();
       const msg = chatForm.value;
-      console.log(msg);
       msg.trim();
       if (!msg) {
         return false;
@@ -85,8 +86,7 @@ window.onload = async () => {
     socket.on("receiveMsg", data => {
       console.log(data);
       outputChattingMsg(data, senderId);
-      console.log(receiverName, senderName);
-      console.log(receiverId, receiverName);
+
       const lastChat = document.querySelector(".status");
       if (data.receiverId == senderId) {
         lastChat.innerHTML = `${data.senderName}：${data.msg}`;
@@ -137,7 +137,6 @@ const createChatting = (data, users, time) => {
 const createChatList2 = (chatType, data, userId1, userId2) => {
   for (const i in data) {
     // sidebar chatroom not onchat
-    console.log(data[i].receiverUserId, userId2, data[i].senderUserId);
     if (data[i].receiverUserId != userId2 && data[i].senderUserId != userId2) {
       const time = toDateFormat(data[i].time);
       let lastChat = "";
@@ -172,66 +171,6 @@ const setHTMLOfChatList = (chatType, data, picture, name, lastChat, time) => {
                     </li>
                     </a>`;
 };
-
-// const createChatList = (chatType, data, users, userId) => {
-//   for (const i in data) {
-//     if (data[0].receiver_id !== userId && data[0].sender_id !== userId) {
-//       const chatList = document.querySelector(".chat-list");
-//       const onChat = document.createElement("a");
-//       onChat.setAttribute("id", chatType);
-//       onChat.href = `./chat.html?room=${data[i].room}`;
-//       chatList.appendChild(onChat);
-//       const listLeft = document.createElement("div");
-//       listLeft.classList.add("list-left");
-//       onChat.appendChild(listLeft);
-//       const profileImg = document.createElement("img");
-//       profileImg.classList.add("list-img");
-//       profileImg.src = "./uploads/images/member.png";
-//       listLeft.appendChild(profileImg);
-//       const listRight = document.createElement("div");
-//       listRight.classList.add("list-right");
-//       onChat.appendChild(listRight);
-//       const listRightUp = document.createElement("div");
-//       listRightUp.classList.add("list-right-up");
-//       listRight.appendChild(listRightUp);
-//       const listRightUpP = document.createElement("p");
-//       listRightUpP.classList.add("list-right-up-p");
-//       listRightUpP.textContent = users.username2;
-//       listRightUp.appendChild(listRightUpP);
-//       if (data[i].not_read > 0) {
-//         const listRightNoti = document.createElement("div");
-//         listRightNoti.classList.add("list-right-notification");
-//         listRightUp.appendChild(listRightNoti);
-//         const listRightNotiP = document.createElement("p");
-//         listRightNotiP.classList.add("list-right-notification-num");
-//         listRightNotiP.textContent = data[i].not_read;
-//         listRightNoti.appendChild("listRightNotiP");
-//       }
-//       const listRightDown = document.createElement("div");
-//       listRightDown.classList.add("list-right-down");
-//       listRight.appendChild(listRightDown);
-//       const listMsg = document.createElement("div");
-//       listMsg.classList.add("list-msg");
-//       listRightDown.appendChild(listMsg);
-//       const listRightDownP = document.createElement("p");
-//       listRightDownP.classList.add("list-right-down-p");
-//       if (data[i].sender_id == users.userId1) {
-//         listRightDownP.textContent = "你：" + data[i].msg;
-//       } else {
-//         listRightDownP.textContent = users.username2 + "：" + data[i].msg;
-//       }
-//       listMsg.appendChild(listRightDownP);
-//       const listDate = document.createElement("div");
-//       listDate.classList.add("list-date");
-//       listRightDown.appendChild(listDate);
-//       const listDateP = document.createElement("p");
-//       listDateP.classList.add("list-date-p");
-//       const time = toDateFormat(data[i].time);
-//       listDateP.textContent = time;
-//       listDate.appendChild(listDateP);
-//     }
-//   }
-// };
 
 const outputMsg = (users, msg) => {
   const receiverPic = document.getElementById("receiver-pic");
